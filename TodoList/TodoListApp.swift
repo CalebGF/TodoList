b42eda6abff27f1fa3989d11548a32a199c148f5
Caleb <caleb2812@gmail.com>
2023-12-15 22:34:12 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Caleb Gonzalez on 15/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
